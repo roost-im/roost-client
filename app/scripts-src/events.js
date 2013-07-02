58e3da2e09b52195c8fe81555dc8856124b6e259
David Benjamin <davidben@mit.edu>
2013-07-01 23:17:18 -0400

    Make RoostEventTarget behave properly for adding/removing mid-dispatch

--- app/scripts-src/events.js
+++ app/scripts-src/events.js
@@ -6,25 +6,40 @@
 function RoostEventTarget() {
   this.listeners_ = {};
 };
+RoostEventTarget.prototype.indexOfListener_ = function(type, cb) {
+  if (!(type in this.listeners_))
+    return -1;
+  for (var i = 0; i < this.listeners_[type].length; i++) {
+    if (this.listeners_[type][i].cb === cb)
+      return i;
+  }
+  return -1;
+};
 RoostEventTarget.prototype.addEventListener = function(type, cb) {
   if (!(type in this.listeners_))
     this.listeners_[type] = [];
-  if (this.listeners_[type].indexOf(cb) == -1)
-    this.listeners_[type].push(cb);
+  if (this.indexOfListener_(type, cb) == -1)
+    this.listeners_[type].push({cb: cb, doomed: false});
 };
 RoostEventTarget.prototype.removeEventListener = function(type, cb) {
   if (!(type in this.listeners_))
     return;
-  var idx = this.listeners_[type].indexOf(cb);
+  var idx = this.indexOfListener_(type, cb);
   if (idx == -1)
     return;
+  // Removing an event listener /does/ affect current dispatches.
+  this.listeners_[type][idx].doomed = true;
   this.listeners_[type].splice(idx, 1);
 };
 RoostEventTarget.prototype.dispatchEvent = function(ev) {
   var type = ev.type;
   if (type in this.listeners_) {
-    for (var i = 0; i < this.listeners_[type].length; i++) {
-      this.listeners_[type][i].call(this, ev);
+    // Make a copy of the list; adding an event listener mid-dispatch
+    // does /not/ run it.
+    var listeners = this.listeners_[type].slice(0);
+    for (var i = 0; i < listeners.length; i++) {
+      if (!listeners[i].doomed)
+        listeners[i].cb.call(this, ev);
     }
   }
 };
